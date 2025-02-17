eebd4f6965247b090420efd14ff88c19c06001fe
Ehisanzarei <ehsanzarei121@gmail.com>
2025-02-17 15:30:49 -0800

    add details

--- js/details.js
+++ js/details.js
@@ -6,16 +6,26 @@ let decreaseBtn = document.querySelector('.decrease')
 let numberOf = document.querySelector('.number')
 let numberLoadingImg = document.querySelector('.number_loading_img')
 let increaseBtn = document.querySelector('.increase')
+let removeIcon = document.querySelector('.remove_icon')
 
 
-decreaseBtn.addEventListener('click',showLodingIcon)
-decreaseBtn.addEventListener('touch',showLodingIcon)
+decreaseBtn.addEventListener('click',showLodingIconAndRemove)
+decreaseBtn.addEventListener('touch',showLodingIconAndRemove)
 increaseBtn.addEventListener('click',showLodingIcon)
 increaseBtn.addEventListener('touch',showLodingIcon)
 
 function showLodingIcon(){
     numberLoadingImg.style.display = "inline"
     numberOf.style.display = "none"
+    removeIcon.style.display = "none"
+    decreaseBtn.style.display = "inline"
+}
+
+function showLodingIconAndRemove(){
+    numberLoadingImg.style.display = "inline"
+    numberOf.style.display = "none"
+    removeIcon.style.display = "inline"
+    decreaseBtn.style.display = "none"
 }
 
 adToBasketBtn.addEventListener('click', function(){
